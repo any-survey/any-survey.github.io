c51039d8dd75400510d4ff5d1d6f90d62b2e4d24
Jay Lee <jaylee.possible@gmail.com>
2022-12-17 22:50:33 +0900

    deploy v2.0.0

--- docs/_next/static/kub9jg6LSxUCFe_2ekEOU/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]\u002F[testCase]","\u002F[locale]\u002F[testCase]\u002Fqna","\u002F[locale]\u002F[testCase]\u002Fresult","\u002F[locale]\u002F[testCase]\u002Fshare"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
